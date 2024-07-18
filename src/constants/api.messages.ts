b12616087c14bad8123cee75af608f2fba3b260f
Kanaan Bahmani <kanaansci@gmail.com>
2024-07-18 13:04:18 +0200

    adding API response enums

---
+++ src/constants/api.messages.ts
@@ -0,0 +1,12 @@
+export enum ApiMessages {
+  INVALID_REQUEST = "Invalid Request",
+  SUCCESS = "Request Successful",
+  CREATED = "Entity Created",
+  UPDATED = "Entity Updated",
+  DELETED = "Entity Deleted",
+  SERVER_ERROR = "Server Error",
+  NOT_FOUND = "Entity Not Found",
+  UNAUTHORIZED = "Unauthorized",
+  ENTITY_ALREADY_EXISTS = "Entity Already Exists",
+  FORBIDDEN = "Forbidden",
+};
